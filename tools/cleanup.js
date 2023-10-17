15dcbfa6825dc8c674998836e6174a8f39cfea60
Błażej Chełkowski <bchelkow@gmail.com>
2023-10-17 09:17:07 +0200

    fix: adjust cleanup script

--- tools/cleanup.js
+++ tools/cleanup.js
@@ -1,8 +1,5 @@
-import fs from 'fs';
-import Path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const fs = require('fs');
+const Path = require('path');
 
 const deleteFolderRecursive = (path) => {
   if (fs.existsSync(path)) {
